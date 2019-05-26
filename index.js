f4a43bca991a9632ef7babfb1f1ae134bcba6381
Gustavo <gugurock50@gmail.com>
2019-05-26 14:03:30 -0300

    some changes

--- index.js
+++ index.js
@@ -6,17 +6,23 @@ const binance = require('node-binance-api')().options({
 });
 
 setInterval(function () {
+    var dateAtual = new Date();
     try{
     binance.balance((error, balances) => {
         if (error) return console.error(error);
         const total = parseFloat(balances.USDT.onOrder) + parseFloat(balances.USDT.available) + parseFloat(balances.TUSD.onOrder) + parseFloat(balances.TUSD.available)
-        console.log("TOTAL BALANCE : ", total, "USD")
-        console.log("INITIAL BALANCE : ", config.INITIAL_INVESTMENT, "USD")
-        console.log("GAINS : ", total - config.INITIAL_INVESTMENT, "USD")
+        console.clear();
+        console.log("==========================================");
+        console.log("SALDO TOTAL..:", total, "USD")
+        console.log("SALDO INCIIAL:", config.INITIAL_INVESTMENT, "USD")
+        console.log("LUCRO........:", total - config.INITIAL_INVESTMENT, "USD")
+        dateAtual = new Date();
+        console.log("@", dateAtual.getHours() + ':' + dateAtual.getMinutes() + ':' + dateAtual.getSeconds());
+        console.log("==========================================");
         var buy = 0.9985;
         var sell = 1.0030;
         binance.prices((error, ticker) => {
-            console.log("Price of BTC: ", ticker.BTCUSDT);
+            console.log("Cotação BTCUSDT", ticker.BTCUSDT);
         });
         binance.prevDay("BTCUSDT", (error, prevDay, symbol) => {
             if (prevDay.priceChangePercent > 0) {
@@ -26,18 +32,18 @@ setInterval(function () {
                 buy = 0.9980;
                 sell = 1.0020;
             }
-            console.log("Entre achat " + buy + " et vente " + sell);
+            console.log("Entre compra " + buy + " e venda " + sell);
             if (balances.USDT.available > 20) {
-                console.log("Achat : " + buy);
+                console.log("Compra: " + buy);
                 binance.buy("TUSDUSDT", ((balances.USDT.available - 0.1) / buy).toFixed(2), buy);
             }
             if (balances.TUSD.available > 20) {
-                console.log("Vente : " + sell);
+                console.log("Venda: " + sell);
                 binance.sell("TUSDUSDT", (balances.TUSD.available - 0.1).toFixed(2), sell);
             }
         });
     });
     }catch($e){
-        console.log("Une erreur est survenue : "+$e);
+        console.log("ERRO : "+$e);
     }
-}, 3000)
+}, 10000)
